cf6740f86443081c4b7eee9acde086c85da6f1bc
zanderson3j <zanderson3j@gmail.com>
2020-01-22 09:02:59 -0700

    jdbc database

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,15 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
-    public TimeEntryRepository inMemoryTimeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository timeEntryRepository() {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     public static void main(String[] args) {
